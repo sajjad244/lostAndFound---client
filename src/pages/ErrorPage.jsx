607d226102485a9577e44ef9b496c726d80e7557
sajjad244 <mrsajjed@gmail.com>
2025-02-10 22:54:57 +0600

    error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,7 @@
 import {Link} from "react-router-dom";
 
 const ErrorPage = () => {
+  //error page
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="text-center">
