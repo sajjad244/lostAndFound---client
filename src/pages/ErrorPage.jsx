e2437ecfd14bfd69464cdb7df8226d3d3b74a7fa
sajjad244 <mrsajjed@gmail.com>
2025-05-11 00:54:30 +0600

    error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,21 +1,18 @@
 import {Link} from "react-router-dom";
+import errorImage from "../assets/error.jpg";
 
 const ErrorPage = () => {
-  //error page
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen ">
       <div className="text-center">
-        <h1 className="text-9xl font-bold text-red-500">404</h1>
+        <img src={errorImage} alt="Error" className="w-64 mx-auto mb-6" />
         <p className="mt-4 text-2xl font-semibold text-gray-800">
           Oops! Page Not Found
         </p>
         <p className="mt-2 text-gray-600">
           The page you’re looking for doesn’t exist or has been moved.
         </p>
-        <Link
-          to="/"
-          className="mt-6 inline-block px-6 py-3 text-white bg-slate-500 rounded-lg hover:bg-blue-700 font-bold"
-        >
+        <Link to="/" className="mt-6 btn btn-outline">
           Back to Home
         </Link>
       </div>
